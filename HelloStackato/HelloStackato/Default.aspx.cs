355cd69f2a1fc6889cf3c8a8665d1bac8fd9ae67
Felix Terkhorn <terkhorn@gmail.com>
2012-08-29 14:20:55 -0400

    add codebehind

---
+++ HelloStackato/HelloStackato/Default.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HelloStackato
+{
+    public partial class HelloPage : Page
+    {
+        private const string ConnString = @"foo";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            databaseInfo.Text = OpenSqlConnection(ConnString);
+        }
+
+        private static string OpenSqlConnection(string connectionString)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                return string.Format("ServerVersion: {0} -- State: {1}", connection.ServerVersion, connection.State);
+            }
+        }
+    }
+}
