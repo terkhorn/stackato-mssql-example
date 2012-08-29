c113825d4259d1a2d53e318b109a72bbf9361cfa
Felix Terkhorn <terkhorn@gmail.com>
2012-08-29 14:41:08 -0400

    Dynamically determine connection string

--- HelloStackato/HelloStackato/Default.aspx.cs
+++ HelloStackato/HelloStackato/Default.aspx.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -10,10 +11,31 @@ namespace HelloStackato
 {
     public partial class HelloPage : Page
     {
-        private const string ConnString = @"foo";
+        private string ConnString { get; set; }
+
+        private string GetConnString()
+        {
+            System.Configuration.Configuration rootWebConfig =
+    System.Web.Configuration.WebConfigurationManager.OpenWebConfiguration("/HelloStackato");
+            System.Configuration.ConnectionStringSettings connString;
+            if (rootWebConfig.ConnectionStrings.ConnectionStrings.Count > 0)
+            {
+                connString =
+                    rootWebConfig.ConnectionStrings.ConnectionStrings["Default"];
+                if (connString != null)
+                {
+                    return connString.ConnectionString;
+                }
+                
+                throw new Exception("No Default connection string");
+            }
+
+            throw new Exception("Can't find connection strings in root web.config");
+        }
 
         protected void Page_Load(object sender, EventArgs e)
         {
+            ConnString = GetConnString();
             databaseInfo.Text = OpenSqlConnection(ConnString);
         }
 
@@ -22,7 +44,7 @@ namespace HelloStackato
             using (var connection = new SqlConnection(connectionString))
             {
                 connection.Open();
-                return string.Format("ServerVersion: {0} -- State: {1}", connection.ServerVersion, connection.State);
+                return string.Format("Current dir -- {2} -- ServerVersion: {0} -- State: {1}", connection.ServerVersion, connection.State, Directory.GetCurrentDirectory());
             }
         }
     }
